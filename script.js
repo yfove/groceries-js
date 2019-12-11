d431107ee63379a1e0a1edf5356dbc908780ad37
curtis chat <curtis.ch.chat@gmail.com>
2019-12-10 23:12:47 -0700

    user can add items

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Document.ready
 var app = {};
 
-app.ul = $(".groceries");
+app.ul = $(".groceries"); //when variables represent a jQuery object
+
+//function for adding items
+app.addItem = function(item){
+    //create list item and add it to .groceries
+    const groceryItem = ` <li>${item}</li>`;
+    app.ul.append(groceryItem);
+}
+
+//new function for adding headings
 
 
 $(function() {
     console.log("linked");
+    // app.addItem('veg'); //test addItem function
 });
 
